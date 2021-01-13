5763bb9bbb59ac99538a523abed2a877783d58f4
whiteted-strats <whiteted.strats@gmail.com>
2021-01-12 22:18:44 -0500

    include LOAD_ALL without HUD libraries

---
+++ LOAD_ALL.lua
@@ -0,0 +1,19 @@
+
+-- If you do need these, an old-er version of HUD_Matt is on my github
+-- require "HUD_Matt\\HUD_matt_core"
+-- require "HUD_Matt\\HUD_matt_lib"
+-- require "HUD_Matt\\matrix_inverse"
+
+require "Data\\GE\\PositionData"
+require "Data\\GE\\PlayerData"
+require "Data\\GE\\ObjectData"
+require "Data\\GE\\GameData"
+require "Data\\GE\\GuardData"
+require "Data\\GE\\PresetData"
+require "Data\\GE\\ScriptData"
+
+require "Utilities\\GE\\ObjectDataReader"
+require "Utilities\\GE\\GuardDataReader"
+require "Utilities\\GE\\IntroDataReader"
+require "Utilities\\GE\\ProjectileDataReader"
+require "Utilities\\GE\\ExplosionDataReader"
